28f4c6f5eef17408571069aec497ef47a7ddff5e
Mukhammadjon <sheykxmuhammad@gmail.com>
2024-04-29 16:58:59 +0500

    commit

--- lib/src/ui/drawer/client/debtbook/debtbook_screen.dart
+++ lib/src/ui/drawer/client/debtbook/debtbook_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:savdo_admin/src/bloc/client/debt_client_bloc.dart';
+import 'package:savdo_admin/src/model/client/clientdebt_model.dart';
 import 'package:savdo_admin/src/theme/colors/app_colors.dart';
 import 'package:savdo_admin/src/theme/icons/app_fonts.dart';
+import 'package:savdo_admin/src/ui/main/main_screen.dart';
 
 class DebtBookScreen extends StatefulWidget {
   const DebtBookScreen({super.key});
@@ -26,34 +28,51 @@ class _DebtBookScreenState extends State<DebtBookScreen> {
         automaticallyImplyLeading: false,
         title: const Text("Қарздорлик китоби")
       ),
-      body: ListView.builder(itemBuilder: (ctx,index){
-        return Container(
-          padding: EdgeInsets.symmetric(vertical: 8.r,horizontal: 16.w),
-          decoration: BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-                      color: Colors.grey.shade300
-                  )
-              ),
-              color: Colors.white
-          ),
-          child: Row(
-            children: [
-              CircleAvatar(
-                child:Text("M",style: AppStyle.mediumBold(Colors.black),),
-              ),
-              SizedBox(width: 8.w,),
-              Expanded(child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Muhammad ali",style: AppStyle.mediumBold(Colors.black),),
-                  Text("123 231 312",style: AppStyle.small(Colors.black),),
-                ],
-              )),
-            ],
-          ),
-        );
-      })
+      body: StreamBuilder<List<DebtClientModel>>(
+        stream: clientDebtBloc.getClientDebtStream,
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            var data = snapshot.data!;
+            return ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (ctx,index){
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 8.r,horizontal: 16.w),
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(
+                            color: Colors.grey.shade300
+                        )
+                    ),
+                    color: Colors.white
+                ),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      backgroundColor: Colors.green.shade100,
+                      child: Text(data[index].name[0]),
+                    ),
+                    SizedBox(width: 8.w,),
+                    Expanded(child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(data[index].name,style: AppStyle.mediumBold(Colors.black),),
+                        Row(
+                          children: [
+                            Text("${priceFormat.format(data[index].osK)} сўм",style: AppStyle.medium(data[index].osK.toString()[0] =='-'?Colors.red:Colors.green),),
+                            Text(" | ",style: AppStyle.medium(Colors.black),),
+                            Text("${priceFormatUsd.format(data[index].osKS)} \$",style: AppStyle.medium(data[index].osKS.toString()[0] =='-'?Colors.red:Colors.green),),
+                          ],
+                        ),
+                      ],
+                    )),
+                  ],
+                ),
+              );
+            });
+          }return const SizedBox();
+        }
+      )
     );
   }
 }
